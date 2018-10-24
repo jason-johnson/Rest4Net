a3b6002188a588b9a04dd86766318c197c5c79da
jason <jason@johnson-net.ch>
2018-10-24 09:10:44 +0200

    Made the initial test

---
+++ Rest4Net.Tests/CoffeeServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Rest4Net.Test.Services;
+using Xunit;
+
+namespace Rest4Net.Tests
+{
+    public class CoffeeServiceTests
+    {
+        [Fact]
+        public void TestGetCoffee()
+        {
+            var service = new CoffeeShop();
+            var coffee = service.OrderCoffee("latte");
+
+            Assert.NotNull(coffee);
+        }
+    }
+}
