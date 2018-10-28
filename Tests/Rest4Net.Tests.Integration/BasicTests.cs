4de5b13962ebc9a3a0dae03b13c75a2339eeb7f4
jason <jason@johnson-net.ch>
2018-10-28 23:33:42 +0100

    Created initial integration tests
    The test is currently failing because no routes are configured.

---
+++ Tests/Rest4Net.Tests.Integration/BasicTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Rest4Net.Tests.Integration
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<WebApi.Startup>>
+    {
+        private readonly WebApplicationFactory<WebApi.Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<WebApi.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/Coffee")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
