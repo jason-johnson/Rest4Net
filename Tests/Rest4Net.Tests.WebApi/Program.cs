b7da80254e68dd4e9e69fdb10cc9910578501390
jason <jason@johnson-net.ch>
2019-12-25 17:53:04 +0100

    Update to modern startup configuration

--- Tests/Rest4Net.Tests.WebApi/Program.cs
+++ Tests/Rest4Net.Tests.WebApi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Rest4Net.Tests.WebApi
 {
@@ -7,11 +8,14 @@ namespace Rest4Net.Tests.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
