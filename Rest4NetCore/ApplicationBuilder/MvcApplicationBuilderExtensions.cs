1a685be075397a19438b5e09da90b8e89136fccd
jason <jason@johnson-net.ch>
2019-12-26 09:37:47 +0100

    Added RestBuilder
    This class will create the routes configuration for the REST code

--- Rest4NetCore/ApplicationBuilder/MvcApplicationBuilderExtensions.cs
+++ Rest4NetCore/ApplicationBuilder/MvcApplicationBuilderExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Rest4NetCore.Attributes;
+using Rest4NetCore.Builder;
 
 namespace Rest4NetCore.ApplicationBuilder
 {
@@ -18,53 +18,25 @@ namespace Rest4NetCore.ApplicationBuilder
 
             var types = LoadAllDefinedTypes();
 
-            var ss = CompileTypes(types);
+            var builder = new RestBuilder(types);
 
             return app.UseEndpoints(endpoints =>
             {
                 // Mapping of endpoints goes here:
                 endpoints.MapControllers();
-                endpoints.MapControllerRoute("coffee", "Coffee/",
-                    defaults: new { controller = "Coffee", action = "GetAll" });
+                builder.MapRestControllers(endpoints);
             });
         }
 
-        private static IEnumerable<TypeInfo> LoadAllDefinedTypes()
+        private static IEnumerable<Type> LoadAllDefinedTypes()
         {
             var assembly = Assembly.GetEntryAssembly();
             var refAssemblies = assembly.GetReferencedAssemblies().Select(Assembly.Load);
 
             return refAssemblies
                 .Concat(new[] { assembly })
-                .SelectMany(x => x.DefinedTypes);
-        }
-
-        private static string CompileTypes(IEnumerable<TypeInfo> types)
-        {
-            foreach (var type in types)
-            {
-                if(IsType(typeof(RestController), type))
-                {
-                    // TODO: Deal with REST controller
-                }
-                else if(HasAttrib(typeof(RestContractAttribute), type))
-                {
-                    // TODO: Deal with REST contract
-                }
-            }
-            return "what do we return";
-        }
-
-        private static bool IsType(Type targ, TypeInfo t)
-        {
-            var ti = targ.GetTypeInfo();
-
-            return ti.IsAssignableFrom(t.AsType()) && ti != t;
-        }
-
-        private static bool HasAttrib(Type attr, TypeInfo t)
-        {
-            return t.AsType().GetCustomAttributes(attr, true).Length > 0;
+                .SelectMany(x => x.DefinedTypes)
+                .Select(x => x.AsType());
         }
     }
 }
---
+++ Rest4NetCore/Builder/RestBuilder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Rest4NetCore.Attributes;
+
+namespace Rest4NetCore.Builder
+{
+    public class RestBuilder
+    {
+        private readonly List<Type> controllers = new List<Type>();
+        private readonly List<Type> contracts = new List<Type>();
+
+        private bool entryPointSeen;
+        private List<Type> restContracts;
+        private List<Type> restModels;
+        private List<Type> restControllers;
+
+        public RestBuilder(IEnumerable<Type> types)
+        {
+            foreach (var type in types)
+            {
+                if (IsType(typeof(RestController), type))
+                {
+                    controllers.Add(type);
+                }
+                else if (HasAttrib(typeof(RestContractAttribute), type))
+                {
+                    contracts.Add(type);
+                }
+            }
+        }
+
+        public void MapRestControllers(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder endpoints)
+        {
+            Build();
+            endpoints.MapControllerRoute("coffee", "Coffee/",
+                defaults: new { controller = "Coffee", action = "GetAll" });
+        }
+
+        private void Build()
+        {
+            entryPointSeen = false;
+            restContracts = new List<Type>();
+            restModels = new List<Type>();
+            restControllers = new List<Type>();
+
+            foreach (var contract in contracts)
+            {
+                var contractInfo = contract.GetCustomAttribute(typeof(RestContractAttribute), true) as RestContractAttribute;
+                restContracts.Add(contract);
+                restModels.Add(contractInfo.ModelClass);
+            }
+
+            foreach(var controller in this.controllers)
+            {
+
+            }
+        }
+
+        private static bool IsType(Type targ, Type t)
+        {
+            var ti = targ.GetTypeInfo();
+
+            return ti.IsAssignableFrom(t) && targ != t;
+        }
+
+        private static bool HasAttrib(Type attr, Type t)
+        {
+            return t.GetCustomAttributes(attr, true).Length > 0;
+        }
+    }
+}
